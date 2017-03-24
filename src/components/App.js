a0f082d435627032805ed7cd77f11bcaade58359
Brian Bycynski <high5bri@hotmail.com>
2017-03-24 07:47:45 -0400

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react'
 class App extends Component {
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <div className='grid'>
+        <header>Header</header>
+        <article>
+          <h1>Article</h1>
+          <p>CONTENT FOR MY EXCELLENT ARTICLE WOULD GO HERE</p>
+        </article>
+        <aside>Aside</aside>
+        <footer>Footer</footer>
+      </div>
     </div>
   }
 }
